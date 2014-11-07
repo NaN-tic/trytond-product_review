70f95a5b3310efcac7a7e3a47355113620c7ee1a
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:31:59 +0100

    Cleanup tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
-#This file is part product_review module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part product_review module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from .test_product_review import suite
 
