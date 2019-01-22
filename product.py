8686e4a10033306ce00de1b66e4223ef56dd4c09
?ngel ?lvarez <angel@nan-tic.com>
2019-01-22 15:32:27 +0100

    Add ir.message and use UserWarning/UserError

--- product.py
+++ product.py
@@ -8,6 +8,7 @@ from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
 from trytond.sendmail import SMTPDataManager, sendmail_transactional
 import logging
+from trytond.i18n import gettext
 
 logger = logging.getLogger(__name__)
 
@@ -91,23 +92,6 @@ class ProductReview(ModelSQL, ModelView):
                     'icon': 'tryton-go-next',
                     },
                 })
-        cls._error_messages.update({
-                'no_smtp_server_defined': ('You must define an SMTP server in '
-                    'order to send scheduled emails warning of new product '
-                    'reviews!'),
-                'check_user_emails': ('No users with email defined in group '
-                    'product review!'),
-                'subject': 'New list of products to review',
-                'body': ('Dear employee,\n\n'
-                    'Here you have the new list of products to be reviewed:\n'
-                    '\n%s\n\n'
-                    'Thank you for your attention and good job!\n\n'
-                    'Sincerely,\n\nthe Management Team.\n\n'
-                    'Note: This messages has been generated and sent '
-                    'automatically, please do not repply.'),
-                'smtp_error': ('Error connecting to SMTP server. '
-                    'Emails have not been sent'),
-                })
 
     @staticmethod
     def default_state():
@@ -167,8 +151,7 @@ class ProductReview(ModelSQL, ModelView):
         if not smtp_servers:
             smtp_servers = SMTPServer.search([('default', '=', True)], limit=1)
         if not smtp_servers:
-            message = cls.raise_user_error('no_smtp_server_defined',
-                raise_exception=False)
+            message = gettext('product_review.no_smtp_server_defined')
             logger.warning(message)
             return
         smtp_server, = smtp_servers
@@ -180,8 +163,7 @@ class ProductReview(ModelSQL, ModelView):
         if (not recipients
                 or (emailvalid
                     and not any(map(emailvalid.check_email, recipients)))):
-            message = cls.raise_user_error('check_user_emails',
-                raise_exception=False)
+            message = gettext('product_review.check_user_emails')
             logger.warning(message)
             return
 
@@ -200,9 +182,8 @@ class ProductReview(ModelSQL, ModelView):
             records = '\n'.join(['%s: %s %s' %
                     (r.date, r.product.name, r.review_type.name)
                     for r in reviews])
-            message = cls.raise_user_error('body', error_args=(records,),
-                raise_exception=False)
-            subject = cls.raise_user_error('subject', raise_exception=False)
+            message = gettext('product_review.body', message=records)
+            subject = gettext('product_review.subject')
 
             from_ = smtp_server.smtp_email
             msg = MIMEText(message, _charset='utf-8')
